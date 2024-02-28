c01b46a621168311510fdfe596b73cfd131e2bc7
Areeb <areeb.wadood9@gmail.com>
2024-02-28 21:32:21 +0530

    healthcheck

--- src/main/java/com/example/TWF/TwfApplication.java
+++ src/main/java/com/example/TWF/TwfApplication.java
@@ -1,13 +1,29 @@
 package com.example.TWF;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwfApplication {
+public class TwfApplication implements HealthIndicator {
+
+	private final long startTime = System.currentTimeMillis();
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwfApplication.class, args);
 	}
 
+	@Override
+	public Health getHealth(boolean includeDetails) {
+		return HealthIndicator.super.getHealth(includeDetails);
+	}
+
+	@Override
+	public Health health() {
+		long uptime = System.currentTimeMillis() - startTime;
+		return Health.up()
+				.withDetail("Uptime", uptime + " ms")
+				.build();
+	}
 }
